cc6a83f68507c6d66ced63a538aa57c08b86396c
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-02-01 08:37:26 -0500

    Creacion de componete Zoom Range

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
-  styleUrls: ['./zoom-range.component.scss']
+  styleUrls: ['./zoom-range.component.scss'],
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef;
 
-  constructor() { }
+  mapa!: mapboxgl.Map;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-74.16428974815396, 4.681994280019149],
+      zoom: 16,
+    });
   }
 
+  zoomOut(): void {
+    this.mapa.zoomOut();
+    console.log('zoomOut', this.divMapa);
+  }
+
+  zoomIn(): void {
+    this.mapa.zoomIn();
+  }
 }

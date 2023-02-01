336caf42e637bb021ed89b94a2dd75e809004df9
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-02-01 12:15:15 -0500

    Componente de mini mapa reutilizable

--- src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
-  styleUrls: ['./mini-mapa.component.scss']
+  styleUrls: ['./mini-mapa.component.scss'],
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef;
 
-  constructor() { }
+  @Input() lngLat: [number, number] = [0, 0];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    const mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false,
+    });
+
+    new mapboxgl.Marker().setLngLat(this.lngLat).addTo(mapa);
   }
-
 }

d3a07e7bc793c450514c860fbcf03fcd8712b7db
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-02-01 09:38:21 -0500

    completo zoom range

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -6,10 +6,13 @@ import * as mapboxgl from 'mapbox-gl';
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.component.scss'],
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy{
   @ViewChild('mapa') divMapa!: ElementRef;
 
   mapa!: mapboxgl.Map;
+
+  zoomLevel: number = 10;
+  center: [number, number] = [-74.16428974815396, 4.681994280019149];
 
   constructor() {}
 
@@ -17,17 +20,43 @@ export class ZoomRangeComponent implements AfterViewInit {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v12',
-      center: [-74.16428974815396, 4.681994280019149],
-      zoom: 16,
+      center: this.center,
+      zoom: this.zoomLevel,
     });
+
+    this.mapa.on('zoom', () => (this.zoomLevel = this.mapa.getZoom()));
+
+    this.mapa.on('zoomend', () => {
+      if (this.mapa.getZoom() > 18) {
+        this.mapa.zoomTo(18);
+      }
+    });
+
+    //* MOVIMIENTO DEL MAPA
+    this.mapa.on('move', (event) => {
+      const target = event.target;
+
+      const { lng, lat } = target.getCenter();
+
+      this.center = [lng, lat];
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () => {})
+    this.mapa.off('zoomend', () => {})
+    this.mapa.off('move', () => {})
   }
 
   zoomOut(): void {
     this.mapa.zoomOut();
-    console.log('zoomOut', this.divMapa);
   }
 
   zoomIn(): void {
     this.mapa.zoomIn();
+  }
+
+  zoomCambio(valor: string): void {
+    this.mapa.zoomTo(Number(valor));
   }
 }
